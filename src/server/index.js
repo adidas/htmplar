c062088be12df8abfad43ac8f928fcd342f991cd
Bilal Çınarlı <bcinarli@gmail.com>
2018-06-08 13:56:14 +0200

    Template updates

--- src/server/index.js
+++ src/server/index.js
@@ -15,23 +15,25 @@ const start = () => {
     app.use(express.static(path.join(process.cwd(), assets)));
 
     app.get('/', (req, res) => {
-        res.status(200).send(
-            '<title>HTMplar</title>\n'+
-            '<div style="background: #fff;font: 16px/1.25 AdiHaus, Helvetica, Arial, sans-serif;">' + 
-                '<div style="width: 100%;'+
-                'max-width: 640px !important;'+
-                'margin: auto;"'+
-                'class="htmplar-landing-page">\n'+
-                    '<h1 style="margin: 32px 16px;" class="htmplar-landing-page-title">My Email Templates</h1>\n'+
-                    '<ul style="list-style-type: none;'+
-                    'padding: 0;'+
-                    'margin: 0"'+
-                    'class="htmplar-landing-page-pages-list">' + pages.join("\n") + '</ul>'+
-                '</div>' +
-            '</div>'
-        );
-    });
-  
+            res.status(200).send(
+                `
+                <title>HTMplar</title>
+                    <div style="background: #fff;font: 16px/1.25 AdiHaus, Helvetica, Arial, sans-serif;">
+                    <div style="width: 100%; max-width: 640px !important; margin: auto;" 
+                        class="htmplar-landing-page">
+                        <h1 style="margin: 32px 16px;" 
+                            class="htmplar-landing-page-title">My Email Templates</h1>
+                        <ul style="list-style-type: none; padding: 0; margin: 0"
+                            class="htmplar-landing-page-pages-list">
+                            ${pages.join('\n')}
+                        </ul>
+                        </div>
+                </div>
+                `
+            );
+        }
+    );
+
     app.listen(server.port || 3000);
 };
 
--- src/server/page.js
+++ src/server/page.js
@@ -13,30 +13,35 @@ const getPage = fileData => {
         let fileDir = fileData.dir.split(output);
         let href = fileDir[fileDir.length - 1];
         let fileName = fileData.dir.split('/');
-        let pageName = fileName[fileName.length - 1].replace("-"," ");
+        let pageName = fileName[fileName.length - 1].replace('-', ' ');
 
         return {
-            pageName: pageName, 
+            pageName: pageName,
             href: href
-        }
+        };
     }
     return null;
-}
+};
 
 const setPage = pageData => {
-    let listItem = 
-    '<li style="padding: 8px 16px 8px 8px;border-bottom: 1px solid #ddd;">'+
-        '<a style="width: 100%;'+
-        'display: inline-block;'+
-        'padding: 8px 16px 8px 8px;'+
-        'position: relative;'+
-        'text-decoration: none;'+
-        'color: #000;" href='+pageData.href+'>'+pageData.pageName+'</a>'+
-    '</li>';
+    let listItem =
+        `<li style="padding: 8px 16px 8px 8px; border-bottom: 1px solid #ddd;">
+            <a style="width: 100%;
+                    display: inline-block;
+                    padding: 8px 16px 8px 8px;
+                    position: relative;
+                    text-decoration: none;
+                    color: #000;" 
+                href="${pageData.href}"
+            >
+                ${pageData.pageName}
+            </a>
+        </li>`;
+
     if (pages.indexOf(listItem) === -1) {
         pages.push(listItem);
     }
-} 
+};
 
 walkSync(output).forEach(file => {
     let page = getPage(file);
