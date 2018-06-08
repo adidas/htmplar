cb4b47d5aae9b21003bb78ae63d9fc2f62a0b033
Beatriz Infante <beatriz.infante@adidas.com>
2018-06-08 13:02:45 +0200

    Landing page

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,11 @@
  * index
  **/
 
+
 const {config: {output, assets, server}} = require('../utils');
 const path = require('path');
 const express = require('express');
-//const page = require('./page');
+const {pages} = require('./page');
 
 const start = () => {
     const app = express();
@@ -14,10 +15,17 @@ const start = () => {
     app.use(express.static(path.join(process.cwd(), assets)));
 
     app.get('/', (req, res) => {
-        res.status(200);
+        res.status(200).send(
+            '<link rel="stylesheet" type="text/css" href="styles/default.css">\n'+
+            '<title>HTMplar</title>\n'+
+            '<div class="wrapper">\n'+
+                '<h1 class="title">My Email Templates</h1>\n'+
+                '<ul class="pages-list">' + pages.join("\n") + '</ul>'+
+            '</div>'
+        );
     });
-
-    app.listen(server.port || 3000);
+  
+    app.listen(server.port || 8080);
 };
 
 module.exports = {
--- src/server/page.js
+++ src/server/page.js
@@ -3,13 +3,37 @@
  **/
 
 const defaultCfg = require('../../.htmplarrc.json');
-const {output} = require('rc')('htmplar', defaultCfg);
+const {output, sharedContent} = require('rc')('htmplar', defaultCfg);
 const {walkSync} = require('../utils');
 
-const page = () => {
-    const files = walkSync(output);
+const pages = [];
 
-    return files;
-};
+const getPage = fileData => {
+    if(fileData.dir !== sharedContent){
+        var fileDir = fileData.dir.split(output);
+        var fileName = fileData.dir.split('/');
+        var pageName = fileName[fileName.length - 1].replace("-"," ");
 
-module.exports = page;
+        return {
+            pageName: pageName, 
+            href: fileDir[fileDir.length - 1]
+        }
+    }
+    return null;
+}
+
+const setPage = pageData => {
+    var listItem = '<li><a href='+pageData.href+'>'+pageData.pageName+'</a></li>';
+    if(pages.indexOf(listItem) === -1){
+        pages.push(listItem);
+    }
+} 
+
+walkSync(output).forEach(file => {
+    var page = getPage(file);
+    if(page){
+        setPage(page);
+    }
+});
+
+module.exports = {pages};
