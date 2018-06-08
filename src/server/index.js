3af0993352442c35278e4edf33a6564a591541fd
Beatriz Infante <beatriz.infante@adidas.com>
2018-06-08 13:03:30 +0200

    landing page styles

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@
 const {config: {output, assets, server}} = require('../utils');
 const path = require('path');
 const express = require('express');
-const {pages} = require('./page');
+const pages = require('./page');
 
 const start = () => {
     const app = express();
@@ -16,16 +16,23 @@ const start = () => {
 
     app.get('/', (req, res) => {
         res.status(200).send(
-            '<link rel="stylesheet" type="text/css" href="styles/default.css">\n'+
             '<title>HTMplar</title>\n'+
-            '<div class="wrapper">\n'+
-                '<h1 class="title">My Email Templates</h1>\n'+
-                '<ul class="pages-list">' + pages.join("\n") + '</ul>'+
+            '<div style="background: #fff;font: 16px/1.25 AdiHaus, Helvetica, Arial, sans-serif;">' + 
+                '<div style="width: 100%;'+
+                'max-width: 640px !important;'+
+                'margin: auto;"'+
+                'class="htmplar-landing-page">\n'+
+                    '<h1 style="margin: 32px 16px;" class="htmplar-landing-page-title">My Email Templates</h1>\n'+
+                    '<ul style="list-style-type: none;'+
+                    'padding: 0;'+
+                    'margin: 0"'+
+                    'class="htmplar-landing-page-pages-list">' + pages.join("\n") + '</ul>'+
+                '</div>' +
             '</div>'
         );
     });
   
-    app.listen(server.port || 8080);
+    app.listen(server.port || 3000);
 };
 
 module.exports = {
--- src/server/page.js
+++ src/server/page.js
@@ -9,31 +9,40 @@ const {walkSync} = require('../utils');
 const pages = [];
 
 const getPage = fileData => {
-    if(fileData.dir !== sharedContent){
-        var fileDir = fileData.dir.split(output);
-        var fileName = fileData.dir.split('/');
-        var pageName = fileName[fileName.length - 1].replace("-"," ");
+    if (fileData.dir !== sharedContent) {
+        let fileDir = fileData.dir.split(output);
+        let href = fileDir[fileDir.length - 1];
+        let fileName = fileData.dir.split('/');
+        let pageName = fileName[fileName.length - 1].replace("-"," ");
 
         return {
             pageName: pageName, 
-            href: fileDir[fileDir.length - 1]
+            href: href
         }
     }
     return null;
 }
 
 const setPage = pageData => {
-    var listItem = '<li><a href='+pageData.href+'>'+pageData.pageName+'</a></li>';
-    if(pages.indexOf(listItem) === -1){
+    let listItem = 
+    '<li style="padding: 8px 16px 8px 8px;border-bottom: 1px solid #ddd;">'+
+        '<a style="width: 100%;'+
+        'display: inline-block;'+
+        'padding: 8px 16px 8px 8px;'+
+        'position: relative;'+
+        'text-decoration: none;'+
+        'color: #000;" href='+pageData.href+'>'+pageData.pageName+'</a>'+
+    '</li>';
+    if (pages.indexOf(listItem) === -1) {
         pages.push(listItem);
     }
 } 
 
 walkSync(output).forEach(file => {
-    var page = getPage(file);
-    if(page){
+    let page = getPage(file);
+    if (page) {
         setPage(page);
     }
 });
 
-module.exports = {pages};
+module.exports = pages;
