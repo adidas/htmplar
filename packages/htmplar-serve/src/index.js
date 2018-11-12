1d3ec80c069638c8785275263a5712c79f7ca4f1
Berkan Dirim <berkandirim@gmail.com>
2018-11-12 10:13:03 +0100

    pages

--- packages/htmplar-serve/src/index.js
+++ packages/htmplar-serve/src/index.js
@@ -1,8 +1,10 @@
 // htmplar server
 
 import path from 'path';
+import { STATUS_CODES } from 'http';
 import express from 'express';
 import { cfg } from './utils';
+import pages from './page';
 
 const start = () => {
   const defaultPort = 3000;
@@ -16,6 +18,28 @@ const start = () => {
 
   app.use(express.static(path.join(appRoot, output)));
   app.use(express.static(path.join(appRoot, assets)));
+
+  app.get('/', (req, res) => {
+    res.status(STATUS_CODES[200]).send(
+      `
+      <title>HTMplar</title>
+      <div style="background: #fff; font: 16px/1.25 AdiHaus, Helvetica, Arial, sans-serif;">
+        <div style="width: 100%; max-width: 640px !important; margin: auto;"
+            class="htmplar-landing-page">
+          <h1 style="margin: 32px 8px;"
+              class="htmplar-landing-page-title">
+            My Email Templates
+          </h1>
+          <ul style="list-style-type: none; padding: 0; margin: 0"
+              class="htmplar-landing-page-pages-list">
+            ${ pages.join('\n') }
+          </ul>
+        </div>
+      </div>
+      `
+    );
+  });
+
   app.listen(server.port || defaultPort);
 };
 
---
+++ packages/htmplar-serve/src/page.js
@@ -0,0 +1,97 @@
+// page
+
+import rc from 'rc';
+import defaults from '../.htmplarrc.json';
+import { walkSync } from '../utils';
+
+const {
+  output,
+  sharedContent
+} = rc('htmplar', defaults);
+
+const pages = [];
+const pageData = {};
+
+const getPage = (fileData) => {
+  if (fileData.dir !== sharedContent) {
+    const fileDir = fileData.dir.split(output);
+    const href = process.platform === 'win32' ? fileDir[fileDir.length - 1].replace(/\\/g, '/') : fileDir[fileDir.length - 1];
+    const fileName = process.platform === 'win32' ? fileData.dir.split('\\') : fileData.dir.split('/');
+    const pageName = fileName[fileName.length - 1].replace(/-/g, ' ');
+
+    const programName = fileName[2] ? fileName[2].replace(/-/g, ' ') : '';
+
+    return {
+      programName,
+      pageName,
+      href
+    };
+  }
+
+  return null;
+};
+
+const setPage = (pageData) => {
+  for (const program in pageData) {
+    if (pageData.hasOwnProperty(program) && program !== '') {
+      const items = [];
+
+      pageData[program].pages.forEach((page) => {
+        const item = `
+                <a style="width: 100%;
+                    display: inline-block;
+                    padding: 8px 16px 8px 8px;
+                    position: relative;
+                    text-decoration: none;
+                    color: #000;"
+                    href="${ page.href }"
+                    target="_blank">
+                  ${ page.pageName }
+                </a>`;
+
+        if (pageData[program].pages.length > 1) {
+          if (pageData[program].pages.indexOf(page) > 0 && items.indexOf(item) === -1) {
+            items.push(item);
+          }
+        } else if (items.indexOf(item) === -1) {
+          items.push(item);
+        }
+      });
+      const listItem =
+                `<li style="padding: 8px 16px 8px 8px; border-bottom: 1px solid #ddd;">
+                <h3>${ program }</h3>
+                ${ items.join('') }
+              </li>`;
+
+      if (pages.indexOf(listItem) === -1) {
+        pages.push(listItem);
+      }
+    }
+  }
+};
+
+walkSync(output).forEach((file) => {
+  const page = getPage(file);
+
+  if (page) {
+    if (!pageData[page.programName]) {
+      pageData[page.programName] = {
+        pages: [
+          {
+            pageName: page.pageName,
+            href: page.href
+          }
+        ]
+      };
+    } else if (pageData[page.programName].pages) {
+      pageData[page.programName].pages.push({
+        pageName: page.pageName,
+        href: page.href
+      });
+    }
+  }
+});
+
+setPage(pageData);
+
+exports.pages = pages;
